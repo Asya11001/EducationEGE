40aefc43f1912855fb324b7be3fcfc520241bb79
Asya11001 <arseniysemenov031203@gmail.com>
2021-10-28 15:21:51 +0300

    array commit

---
+++ education_new_things/array.py
@@ -0,0 +1,23 @@
+
+
+
+'''
+
+array = [1, 2, 5, '123', [1, '2']]
+
+
+for i in range(0, len(array)):
+    print(array[i])
+
+'''
+
+
+
+array = [1, 5, 21, 12, 4]
+a = 5
+c = 1
+
+print(array)
+
+print(max(array))
+print(min(array))
